333eb7271d9b2906a76a2d4fde9832f5273e1beb
Iv1nko <ivanko170707@gmail.com>
2022-04-16 22:14:43 +0500

    bucket

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,104 +1,66 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
 import java.util.Iterator;
 
-public class MyGdxGame extends ApplicationAdapter {
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Sound dropSound;
-	private Music rainMusic;
+class GameScreen implements Screen {
+	final Drop game;
 
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
+	Texture dropImage;
+	Texture bucketImage;
+	Sound dropSound;
+	Music rainMusic;
+	OrthographicCamera camera;
+	Rectangle bucket;
+	Array<Rectangle> raindrops;
+	long lastDropTime;
+	int dropsGathered;
 
-	private Rectangle bucket;
+	public GameScreen(final Drop gam) {
+		this.game = gam;
 
-	private Array<Rectangle> raindrops;
-	private long lastDropTime;
-
-	@Override
-	public void create(){
+		// загрузка изображений для капли и ведра, 64x64 пикселей каждый
 		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.pmg"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
 
+		// загрузка звукового эффекта падающей капли и фоновой "музыки" дождя
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
 		rainMusic.setLooping(true);
-		rainMusic.play();
 
+		// создает камеру
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false,800,400);
+		camera.setToOrtho(false, 800, 480);
 
-		batch = new SpriteBatch();
-
+		// создается Rectangle для представления ведра
 		bucket = new Rectangle();
+		// центрируем ведро по горизонтали
 		bucket.x = 800 / 2 - 64 / 2;
+		// размещаем на 20 пикселей выше нижней границы экрана.
 		bucket.y = 20;
+
 		bucket.width = 64;
 		bucket.height = 64;
 
+		// создает массив капель и возрождает первую
 		raindrops = new Array<Rectangle>();
 		spawnRaindrop();
+
 	}
 
-	@Override
-	public void render(){
-		ScreenUtils.clear(0,0,0.2f,1);
-		camera.update();
-
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-			batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
-
-		if(Gdx.input.isTouched()){
-			Vector3 touchpos = new Vector3();
-			touchpos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchpos);
-			bucket.x = touchpos.x - 64 / 2;
-		}
-
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-
-		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-
-			if (raindrop.y + 64 < 0) iter.remove();
-
-			if (raindrop.overlaps(bucket)) {
-				dropSound.play();
-				iter.remove();
-			}
-		}
-	}
-
-	private void spawnRaindrop(){
+	private void spawnRaindrop() {
 		Rectangle raindrop = new Rectangle();
 		raindrop.x = MathUtils.random(0, 800 - 64);
 		raindrop.y = 480;
@@ -108,5 +70,98 @@ public class MyGdxGame extends ApplicationAdapter {
 		lastDropTime = TimeUtils.nanoTime();
 	}
 
+	@Override
+	public void render(float delta) {
+		// очищаем экран темно-синим цветом.
+		// Аргументы для glClearColor красный, зеленый
+		// синий и альфа компонент в диапазоне [0,1]
+		// цвета используемого для очистки экрана.
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 
+		// сообщает камере, что нужно обновить матрицы.
+		camera.update();
+
+		// сообщаем SpriteBatch о системе координат
+		// визуализации указанных для камеры.
+		game.batch.setProjectionMatrix(camera.combined);
+
+		// начитаем новую серию, рисуем ведро и
+		// все капли
+		game.batch.begin();
+		game.font.draw(game.batch, "Drops Collected: " + dropsGathered, 0, 480);
+		game.batch.draw(bucketImage, bucket.x, bucket.y);
+		for (Rectangle raindrop : raindrops) {
+			game.batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		game.batch.end();
+
+		// обработка пользовательского ввода
+		if (Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+		if (Gdx.input.isKeyPressed(Keys.LEFT))
+			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.RIGHT))
+			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		// убедитесь, что ведро остается в пределах экрана
+		if (bucket.x < 0)
+			bucket.x = 0;
+		if (bucket.x > 800 - 64)
+			bucket.x = 800 - 64;
+
+		// проверка, нужно ли создавать новую каплю
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+			spawnRaindrop();
+
+		// движение капли, удаляем все капли выходящие за границы экрана
+		// или те, что попали в ведро. Воспроизведение звукового эффекта
+		// при попадании.
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while (iter.hasNext()) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + 64 < 0)
+				iter.remove();
+			if (raindrop.overlaps(bucket)) {
+				dropsGathered++;
+				dropSound.play();
+				iter.remove();
+			}
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+		// воспроизведение фоновой музыки
+		// когда отображается экрана
+		rainMusic.play();
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+	}
 }
